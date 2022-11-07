199924168740acc5fe783a14be0ee7d02b855673
Krishan Kumar Pareek <krishan.pareek@tavant.com>
2022-11-07 15:59:33 +0530

    Update env.prod config

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB32eCsm_XLPj6XpZcAyfRqXfcXlkw5NGo",
+    authDomain: "dhyan-aasan.firebaseapp.com",
+    projectId: "dhyan-aasan",
+    storageBucket: "dhyan-aasan.appspot.com",
+    messagingSenderId: "180852871951",
+    appId: "1:180852871951:web:b4fea19386492d2d55647a",
+    measurementId: "G-KH4HWXC1LQ"
+  }
 };
